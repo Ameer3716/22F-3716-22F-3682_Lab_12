ad9a6e72c21c5be612fff3a16a212b29d308dbec
Ameer3716 <ameersultan0310@gmail.com>
2024-11-15 20:45:54 +0500

    main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
-}
-
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
+	public static void main(String[] args) {
+        System.out.println("Feature A work in progress.");
+    }
 }
